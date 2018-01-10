b049a6e1939f2dff7337232ef6042b7fc17204e5
Hoang Bui <hoangxuanbui6572@gmail.com>
2018-01-10 15:01:07 -0500

    Sensor files

---
+++ sensor_detection.ino
@@ -0,0 +1,25 @@
+long duration = 5;
+int distance;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(9, OUTPUT);
+  pinMode(10, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(9 ,LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(9, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(9, LOW);
+
+  duration = pulseIn(10, HIGH);
+  distance = duration*0.034/2;
+
+  Serial.print("Distance:");
+  Serial.println(distance);
+}
